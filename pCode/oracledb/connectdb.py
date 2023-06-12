7434d6f2b0dac10ad44d0bae7490bef7ddc69137
shekharnotfound2019 <shekhar.csm@gmail.com>
2023-06-13 00:58:51 +0530

    Jun-12

---
+++ pCode/oracledb/connectdb.py
@@ -0,0 +1,25 @@
+#/usr/bin/env python3
+import getpass
+import oracledb
+
+pw = getpass.getpass("Enter password: ")
+
+connection = oracledb.connect(
+    user="shekhar",
+    password=pw,
+    dsn="oradb1.demo.com/freepdb1")
+
+print("Successfully connected to Oracle Database")
+
+cursor = connection.cursor()
+
+sqlcmd = "select file_name from dba_data_files"
+cursor.execute(sqlcmd)
+all_db_files = cursor.fetchall()
+
+# print all db files
+for dbfile in all_db_files:
+    print(f'{dbfile}')
+print(all_db_files)
+
+connection.close()
